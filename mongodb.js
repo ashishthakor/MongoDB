2079ac8101de0456fceaa178f763b62a4de807f3
ashishthakor <ashuthakor927@gmail.com>
2022-01-25 12:59:21 +0530

    Add Id to single inserted record

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,17 @@
 // CRUD operation
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -15,63 +22,42 @@ MongoClient.connect(
     }
     // console.log('Connected Correctly');
     const db = client.db(databaseName);
-    // db.collection('users').insertOne(
-    //   {
-    //     name: 'Ashish',
-    //     age: 21,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log('Connection Error');
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
-    //     db.collection('users').insertMany(
-    //       [
-    //         {
-    //           name: 'Captain America',
-    //           age: 170,
-    //         },
-    //         {
-    //           name: 'Thor',
-    //           age: 1500,
-    //         },
-    //         {
-    //           name: 'Iron Man',
-    //           age: 40,
-    //         },
-    //       ],
-    //       (error, result) => {
-    //         if (error) {
-    //           return console.log('Unable to insert');
-    //         }
-    //         console.log(result.ops);
-    //       }
-    //     );
-    db.collection('tasks').insertMany(
-      [
-        {
-          descreption: 'Clean the house',
-          completed: true,
-        },
-        {
-          descreption: 'Renew Insplection',
-          completed: false,
-        },
-        {
-          descreption: 'Pot Plants',
-          completed: false,
-        },
-      ],
+    db.collection('users').insertOne(
+      {
+        _id: id,
+        name: 'Hulk',
+        age: 55,
+      },
       (error, result) => {
         if (error) {
-          return console.log(
-            'Unable to insert many records in NewTask Collection'
-          );
+          return console.log('Connection Error');
         }
         console.log(result.ops);
       }
     );
+    // db.collection('tasks').insertMany(
+    //   [
+    //     {
+    //       descreption: 'Clean the house',
+    //       completed: true,
+    //     },
+    //     {
+    //       descreption: 'Renew Insplection',
+    //       completed: false,
+    //     },
+    //     {
+    //       descreption: 'Pot Plants',
+    //       completed: false,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log(
+    //         'Unable to insert many records in NewTask Collection'
+    //       );
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
   }
 );
