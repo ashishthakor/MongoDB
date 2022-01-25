77f825073bb127ce2423ddb9d674de01668328d8
ashishthakor <ashuthakor927@gmail.com>
2022-01-25 17:25:30 +0530

    Creating mongoose model Challenge

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,6 +7,7 @@ mongoose.connect('mongodb://127.0.0.1/task-manager-api', {
   //   The error is because of the new version of the mongoose i.e version 6.0. 6. useNewUrlParser , useUnifiedTopology , useFindAndModify , and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser , useUnifiedTopology , and useCreateIndex are true, and useFindAndModify is false.
 });
 
+// Defining the model
 const User = mongoose.model('User', {
   name: {
     type: String,
@@ -16,14 +17,41 @@ const User = mongoose.model('User', {
   },
 });
 
+// Creating a new Instances
 const me = new User({
   name: 'Ashish',
   age: 'thakor',
 });
 
+// saving the instance
 me.save()
   .then(() => {
     console.log(me);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//------------------------------challenge--------------------------------------
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+const task = new Task({
+  description: 'Learn Mongoose Library',
+  completed: false,
+});
+
+task
+  .save()
+  .then(() => {
+    console.log(task);
   })
   .catch((error) => {
     console.log(error);
