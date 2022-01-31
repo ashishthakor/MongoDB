15df7bcbf7382532f180fff66909a36564e29c61
ashishthakor <ashuthakor927@gmail.com>
2022-01-31 16:21:05 +0530

    Delete Resource from User and Task(By their id)

--- src/index.js
+++ src/index.js
@@ -71,6 +71,20 @@ app.patch('/user/:id', async (req, res) => {
   }
 });
 
+// Delete User
+app.delete('/user/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 // Create Tasks
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
@@ -127,6 +141,18 @@ app.patch('/task/:id', async (req, res) => {
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+// Delete Tasks
+app.delete('/task/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
