7035c18cf8d15a5bea3230b7d9de99dcfe328771
ashishthakor <ashuthakor927@gmail.com>
2022-01-31 16:59:31 +0530

    Saparate Route files

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,28 +1,3 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
-
-// const Task = mongoose.model('Task', {
-//   description: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false,
-//   },
-// });
-
-// const task = new Task({
-//   description: '                    extra Learn Mongoose Library',
-//   completed: true,
-// });
-// task
-//   .save()
-//   .then(() => {
-//     console.log(task);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
---
+++ src/routers/user.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/user');
+
+// Create Users
+router.post('/users', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// Read All Users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// Read Users by their ID
+router.get('/users/:id', async (req, res) => {
+  //   console.log(req.params);
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// Update user by their ID
+router.patch('/user/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid Updates' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// Delete User
+router.delete('/user/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
