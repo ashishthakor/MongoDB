1ed99d1cf2e59136fdb01efcdd707e736c4e0028
ashishthakor <ashuthakor927@gmail.com>
2022-02-01 10:55:24 +0530

    Generateing Authentication Token

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -39,7 +40,24 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+// Method for Generating Token, we are calling it for specific user
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisisnewtoken');
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
 // Checking login credential of user
 userSchema.statics.findByCredentials = async (email, password) => {
--- src/routers/user.js
+++ src/routers/user.js
@@ -7,7 +7,8 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -20,7 +21,8 @@ router.post('/user/login', async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken(); // We are using it on Perticular user so that we have use instance 'user' not Collection of 'User'
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
