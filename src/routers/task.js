1bbdd69835fc716ad47cb1e2de6945ad48ca8b94
ashishthakor <ashuthakor927@gmail.com>
2022-02-01 18:27:32 +0530

    Sorting the Task data based on 'asc' OR 'desc'

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,11 +19,17 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // Read All Tasks --> GET /tasks?completed = true|false
-// Get /tasks?limit=10&skip=0
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt:asc|createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
+  const sort = {};
   if (req.query.completed) {
     match.completed = req.query.completed === 'true';
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
   }
   try {
     await req.user.populate({
@@ -32,6 +38,11 @@ router.get('/tasks', auth, async (req, res) => {
       options: {
         limit: parseInt(req.query.limit),
         skip: parseInt(req.query.skip),
+        sort,
+        // sort: {
+        //   createdAt: 1, // 1 for Ascending and -1 for descending
+        //   // completed: 1,
+        // },
       },
     });
 
