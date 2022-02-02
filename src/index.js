de40e18ab14857a13586da3ed4b58a564b950714
ashishthakor <ashuthakor927@gmail.com>
2022-02-02 12:21:40 +0530

    Practice Validating File Upload

--- src/index.js
+++ src/index.js
@@ -12,6 +12,17 @@ const app = express();
 const multer = require('multer');
 const upload = multer({
   dest: 'images',
+  limits: {
+    fileSize: 1000000, //limit by file size
+  },
+  fileFilter(req, file, cb) {
+    // if (!file.originalname.endsWith('.pdf')) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload a Word Document'));
+    }
+
+    cb(undefined, true);
+  },
 });
 app.post('/upload', upload.single('upload'), (req, res) => {
   res.send();
