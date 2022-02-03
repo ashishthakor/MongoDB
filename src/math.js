af3b609b3b67c7a8e691b3a446858e98e5ee9f1f
ashishthakor <ashuthakor927@gmail.com>
2022-02-03 16:50:15 +0530

    Testing Asyncronous Code

--- src/math.js
+++ src/math.js
@@ -7,8 +7,20 @@ const celsiusToFahrenheit = (temp) => {
   return temp * 1.8 + 32;
 };
 
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject('Number must not be Nagative');
+      }
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
 module.exports = {
   calcTip,
   celsiusToFahrenheit,
   fahrenheitToCelsius,
+  add,
 };
--- test/math.test.js
+++ test/math.test.js
@@ -2,6 +2,7 @@ const {
   calcTip,
   celsiusToFahrenheit,
   fahrenheitToCelsius,
+  add,
 } = require('../src/math');
 
 test('Should calculate total with tip', () => {
@@ -24,10 +25,21 @@ test('Should calculate fahrenheit 32 To Celsius 0', () => {
   expect(temp).toBe(0);
 });
 
-//
-// Goal: Test temperature conversion functions
-//
-// 1. Export both functions and load them into test suite
-// 2. Create "Should convert 32 F to 0 C"
-// 3. Create "Should convert 0 C to 32 F"
-// 4. Run the Jest to test your work!
+// test('Async test Demo', (done) => {
+//   setTimeout(() => {
+//     expect(1).toBe(2);
+//     done();
+//   }, 2000);
+// });
+
+test('should add 2 number', (done) => {
+  add(2, 3).then((sum) => {
+    expect(sum).toBe(5);
+    done();
+  });
+});
+
+test('should add two numbers async/await', async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
+});
